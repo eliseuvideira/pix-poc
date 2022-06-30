c8565e0829f810cf4982e2f0be4f0cdc26ab662f
Eliseu Videira <eliseuvideira1@gmail.com>
2022-06-30 20:21:21 -0300

    :wrench: before eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QrCodePix } from "qrcode-pix";
+import React, { useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Row,
+} from "react-bootstrap";
 
-function App() {
+export const App = () => {
+  const [fields, setFields] = useState({
+    key: "",
+    name: "",
+    city: "",
+    message: "",
+    cep: "",
+    value: "0",
+  });
+  const [qrCode, setQrCode] = useState<string | null>(null);
+
+  const onChange = (key: any) => (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFields({ ...fields, [key]: e.target.value });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col xs="8">
+            <FormGroup>
+              <FormLabel>Key</FormLabel>
+              <FormControl value={fields.key} onChange={onChange("key")} />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Nome</FormLabel>
+              <FormControl value={fields.name} onChange={onChange("name")} />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Cep</FormLabel>
+              <FormControl value={fields.cep} onChange={onChange("cep")} />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Cidade</FormLabel>
+              <FormControl value={fields.city} onChange={onChange("city")} />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Mensagem</FormLabel>
+              <FormControl
+                value={fields.message}
+                onChange={onChange("message")}
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Valor</FormLabel>
+              <FormControl
+                type="number"
+                value={fields.value}
+                onChange={onChange("value")}
+              />
+            </FormGroup>
+            <Button
+              type="button"
+              onClick={async () => {
+                const response = QrCodePix({
+                  version: "01",
+                  key: fields.key,
+                  name: fields.name,
+                  city: fields.city,
+                  message: fields.message,
+                  cep: fields.cep,
+                  value: +fields.value,
+                });
+
+                const qrCode = await response.base64();
+
+                setQrCode(qrCode);
+              }}
+            >
+              Criar QRCode
+            </Button>
+          </Col>
+          <Col xs="4">{qrCode && <img src={qrCode} alt="qrcode" />}</Col>
+        </Row>
+      </Container>
     </div>
   );
-}
-
-export default App;
+};
